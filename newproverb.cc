299190c209499edbb2dc4f7b385ed97cb7793355
Beatrix <bealearnscode@gmail.com>
2019-02-20 02:29:51 +0000

    expanded writeProverb funtion to print the correct output based on number

--- newproverb.cc
+++ newproverb.cc
@@ -3,30 +3,32 @@
 // "Now is the time for all good men to come to the aid of their _______" 
 // Inputting a 1 will use the word party. Any other number will use the word //country. 
 
-// PLACE YOUR NAME HERE 
+// Beatrix House 
 
 #include <iostream> 
 #include <string> 
 using namespace std; 
 
 // Fill in the prototype of the function writeProverb. 
+void writeProverb(int);
 
 int main () 
 { 
-int wordCode; 
-
-cout << "Given the phrase:" << endl; 
-cout << "Now is the time for all good men to come to the aid of their " 
-     		<< endl; 
-cout << "Input a 1 if you want the sentence to be finished with party" 
-     		<< endl; 
-cout << "Input any other number for the word country" << endl; 
-cout << "Please input your choice now" << endl; 
-cin >> wordCode; 
-cout << endl; 
-writeProverb(wordCode); 
-
-return 0; 
+    
+    int wordCode; 
+    
+    cout << "Given the phrase:" << endl; 
+    cout << "'Now is the time for all good men to come to the aid of their ___.' " 
+         		<< endl; 
+    cout << "Input a 1 if you want the sentence to be finished with 'party'" 
+         		<< endl; 
+    cout << "Input any other number for the word 'country'" << endl; 
+    cout << "Please input your choice now" << endl; 
+    cin >> wordCode; 
+    cout << endl; 
+    writeProverb(wordCode); 
+    
+    return 0; 
 } 
 
 // ***************************************************************************** 
@@ -43,8 +45,13 @@ return 0;
 // ***************************************************************************** 
 void writeProverb (int number) 
 { 
-// Fill in the body of the function to accomplish what is described above 
-if (number == 1) {
-    cout <
-}
+    // Fill in the body of the function to accomplish what is described above 
+    if (number == 1) 
+    {
+        cout << "Now is the time for all good men to come to the aid of their party." << endl;
+    }
+    else 
+    {
+        cout << "Now is the time for all good men to come to the aid of their country." << endl;
+    }
 } 
